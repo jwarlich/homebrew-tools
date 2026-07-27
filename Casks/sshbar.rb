a48bcd2911ea6e894f4c2097282c55d0a0f8a252
jwarlich <codebygizmo@gmail.com>
2026-07-27 23:07:33 +0200

    Fix SHA

--- Casks/sshbar.rb
+++ Casks/sshbar.rb
@@ -1,6 +1,6 @@
 cask "sshbar" do
   version "1.0.0"
-  sha256 "<SHA256_FROM_STEP_4>"
+  sha256 "1bb039e5ec6e44d712ebc28a683d038f2bd2bb1d592cbaebac1395d4a8ddb7a4"
 
   url "https://github.com/jwarlich/sshbar/releases/download/v#{version}/SSHBar-#{version}.zip"
   name "SSHBar"
